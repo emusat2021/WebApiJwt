af7a2b351bb0baa21d33f35da01451e59572db85
Eugenia <musat.eugenia@gmail.com>
2024-05-29 20:35:51 +0200

    Add model

---
+++ Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiJwt.Models
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime DateOfJoing { get; set; }
+    }
+}
